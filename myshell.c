094663e617fddda3049fdc5a58ac1e9d39ab1c58
Roman Zelenin <roman_zelenin@hotmail.com>
2024-02-20 16:28:57 +0300

    Added cd command

--- myshell.c
+++ myshell.c
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/param.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#define IS_EQUAL_STR(str1, str2) (strlen(str1) == strlen(str2) && strcmp(str1, str2) == 0)
 
 typedef enum bool
 {
@@ -43,7 +47,9 @@ void clear_buffer(char *buff, size_t n)
 void display_prompt(const char **env)
 {
     const char *username = get_env_value(env, "LOGNAME");
-    const char *pwd = get_env_value(env, "PWD");
+    char *pwd = (char *)calloc(100, 1);
+    getcwd(pwd, 100);
+
     const char *home_path = get_env_value(env, "HOME");
     const char *hostname = get_env_value(env, "NAME");
 
@@ -56,6 +62,7 @@ void display_prompt(const char **env)
         char *tmp = (char *)malloc(tmp_size);
         tmp[0] = '~';
         strncpy(tmp + 1, pwd + len_home_path, tmp_size - 1);
+        free(pwd);
         pwd = tmp;
     }
 
@@ -67,18 +74,19 @@ void display_prompt(const char **env)
     {
         printf("\033[01;32m%s@%s\033[00m:%s$ ", username, hostname, pwd);
     }
+
+    free(pwd);
 }
 
 void exec_command(char const *command)
 {
     if (command != NULL)
     {
-        size_t len_command = strlen(command);
-        if (len_command == strlen("exit") && strcmp(command, "exit") == 0)
+        if (IS_EQUAL_STR(command, "exit"))
         {
             exit(0);
         }
-        else if (len_command == strlen("clear") && strcmp(command, "clear") == 0)
+        else if (IS_EQUAL_STR(command, "clear"))
         {
             printf("\033[1;1H");
             printf("\033[2J");
@@ -86,7 +94,54 @@ void exec_command(char const *command)
         }
         else
         {
-            printf("%s: command not found\n", command);
+            char *name = strtok(command, " ");
+            const size_t MAX_COUNT_ARG = 20;
+            char *arguments[MAX_COUNT_ARG + 1];
+            size_t index = 0;
+
+            char *token = strtok(NULL, " ");
+            while (token && index < MAX_COUNT_ARG)
+            {
+                arguments[index++] = token;
+                token = strtok(NULL, " ");
+            }
+            arguments[index] = NULL;
+
+            if (IS_EQUAL_STR(name, "cd"))
+            {
+                if (index > 1)
+                {
+                    printf("cd: too many arguments\n");
+                }
+                else if (index == 1)
+                {
+                    char *path = arguments[0];
+                    int res = chdir(path);
+
+                    if (res != -1)
+                    {
+                        switch (res)
+                        {
+                        case ENOTDIR:
+                            printf("cd: Not a directory\n");
+                            break;
+                        case ENOENT:
+                            printf("No such file or directory\n");
+                            break;
+                        default:
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        printf("cd: Not a directory\n");
+                    }
+                }
+            }
+            else
+            {
+                printf("%s: command not found\n", command);
+            }
         }
     }
 }
