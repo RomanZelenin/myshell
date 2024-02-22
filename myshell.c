b64ee3ec56fa2387c64f12dcfac61da8ce205078
Roman Zelenin <roman_zelenin@hotmail.com>
2024-02-22 15:19:28 +0300

    Refactoring and fix bugs

--- myshell.c
+++ myshell.c
@@ -9,32 +9,11 @@
 #include <errno.h>
 #include <sys/wait.h>
 
-#define IS_EQUAL_STR(str1, str2) (strlen(str1) == strlen(str2) && strcmp(str1, str2) == 0)
-
 typedef enum bool
 {
     FALSE,
     TRUE
 } bool;
-
-char const *get_env_value(char const *env[], char const *name)
-{
-    int numEnvItems = 0;
-    char const *value = NULL;
-    int nameLength = strlen(name);
-
-    while (env[numEnvItems] != NULL)
-    {
-        int len = (int)(strchr(env[numEnvItems], '=') - env[numEnvItems]);
-        if (nameLength == len && strncmp(name, env[numEnvItems], len) == 0)
-        {
-            value = env[numEnvItems] + len + 1;
-            break;
-        }
-        numEnvItems++;
-    }
-    return value;
-}
 
 void clear_buffer(char *buff, size_t n)
 {
@@ -45,21 +24,38 @@ void clear_buffer(char *buff, size_t n)
     }
 }
 
-void display_prompt(const char **env)
+char const *trim(char *str)
 {
-    const char *username = get_env_value(env, "LOGNAME");
-    char *pwd = (char *)calloc(100, 1);
-    getcwd(pwd, 100);
+    int s = 0;
+    while (s < strlen(str) - 1 && (str[s] == ' '))
+        s++;
 
-    const char *home_path = get_env_value(env, "HOME");
-    const char *hostname = get_env_value(env, "NAME");
+    int e = (int)strlen(str) - 1;
+    while (e >= s && e >= 0 && (str[e] == '\n' || str[e] == ' ' || str[e] == '\r'))
+        e--;
 
-    int len_pwd = strlen(pwd);
-    int len_home_path = strlen(home_path);
-
-    if (len_pwd >= len_home_path && strncmp(pwd, home_path, MIN(len_pwd, len_home_path)) == 0)
+    if (e >= s)
     {
-        size_t tmp_size = len_pwd - len_home_path + 2;
+        str[e + 1] = '\0';
+        return str + s;
+    }
+    return NULL;
+}
+
+void display_prompt()
+{
+    char username[256];
+    cuserid(username);
+
+    char *pwd = getcwd(NULL, 0);
+    const char *home_path = getenv("HOME");
+    char hostname[255];
+    gethostname(hostname, 255);
+
+    int len_home_path = strlen(home_path);
+    if (strncmp(pwd, home_path, len_home_path) == 0)
+    {
+        size_t tmp_size = strlen(pwd) - len_home_path + 2;
         char *tmp = (char *)malloc(tmp_size);
         tmp[0] = '~';
         strncpy(tmp + 1, pwd + len_home_path, tmp_size - 1);
@@ -67,7 +63,7 @@ void display_prompt(const char **env)
         pwd = tmp;
     }
 
-    if (strlen(username) == strlen("root"))
+    if (getuid() == 0)
     {
         printf("%s@%s:%s# ", username, hostname, pwd);
     }
@@ -83,11 +79,11 @@ void exec_command(char const *command, const char **env)
 {
     if (command != NULL)
     {
-        if (IS_EQUAL_STR(command, "exit"))
+        if (strcmp(command, "exit") == 0)
         {
             exit(0);
         }
-        else if (IS_EQUAL_STR(command, "clear"))
+        else if (strcmp(command, "clear") == 0)
         {
             printf("\033[1;1H");
             printf("\033[2J");
@@ -97,7 +93,7 @@ void exec_command(char const *command, const char **env)
         {
             char *name = strtok(command, " ");
             const size_t MAX_COUNT_ARG = 20;
-            char **arguments = (char **)calloc(MAX_COUNT_ARG + 1, sizeof(char*));
+            char **arguments = (char **)calloc(MAX_COUNT_ARG + 1, sizeof(char *));
             size_t index = 0;
 
             char *token = strtok(NULL, " ");
@@ -108,7 +104,7 @@ void exec_command(char const *command, const char **env)
             }
             arguments[index] = NULL;
 
-            if (IS_EQUAL_STR(name, "cd"))
+            if (strcmp(name, "cd") == 0)
             {
                 if (index > 1)
                 {
@@ -119,7 +115,7 @@ void exec_command(char const *command, const char **env)
                     char *path = arguments[0];
                     if (path[0] == '~')
                     {
-                        const char *home_path = get_env_value(env, "HOME");
+                        const char *home_path = getenv("HOME");
                         size_t len_path = strlen(path + 1) + strlen(home_path) + 1;
                         char *buff_path = (char *)calloc(len_path, 1);
                         strcat(buff_path, home_path);
@@ -147,14 +143,14 @@ void exec_command(char const *command, const char **env)
                     {
                         printf("cd: Not a directory\n");
                     }
-                    if (path!=arguments[0])
+                    if (path != arguments[0])
                     {
-                         free(path);
-                    }             
-                   free(arguments);
+                        free(path);
+                    }
+                    free(arguments);
                 }
             }
-            else if (IS_EQUAL_STR(name, "ls"))
+            else if (strcmp(name, "ls") == 0)
             {
                 pid_t pid = fork();
                 if (pid == 0)
@@ -188,24 +184,6 @@ void exec_command(char const *command, const char **env)
     }
 }
 
-char const *trim(char *str)
-{
-    int s = 0;
-    while (s < strlen(str) - 1 && (str[s] == ' '))
-        s++;
-
-    int e = (int)strlen(str) - 1;
-    while (e >= s && e >= 0 && (str[e] == '\n' || str[e] == ' ' || str[e] == '\r'))
-        e--;
-
-    if (e >= s)
-    {
-        str[e + 1] = '\0';
-        return str + s;
-    }
-    return NULL;
-}
-
 int main(int argc, char const *argv[], char const *env[])
 {
     const size_t BUFF_SIZE = 255;
@@ -214,7 +192,7 @@ int main(int argc, char const *argv[], char const *env[])
     setbuf(stdout, NULL);
     while (TRUE)
     {
-        display_prompt(env);
+        display_prompt();
         clear_buffer(buffer, BUFF_SIZE);
         read(STDIN_FILENO, buffer, 255);
         exec_command(trim(buffer), env);
